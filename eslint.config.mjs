b58689281616e60d6f98772b62675faa91754247
Joaoof <joaodeus400@gmail.com>
2025-02-06 15:44:54 -0300

    Update ESLint configuration and dependencies; fix JSON formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules({
+    "@typescript-eslint/no-unused-vars": "off",
+    "compat/compat": "off",
+  }),
 ];
 
 export default eslintConfig;
